34a4158345cadd46fd178fc294f228afd4e9b063
vincent55312 <vincent.vuylsteker@protonmail.com>
2022-06-20 18:49:23 +0200

    refacto

--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,20 @@ import * as express from 'express';
 import * as userRepository from './repository/user-repository';
 import * as bodyParser from 'body-parser';
 import { User } from './entity/user';
+import * as dotenv from 'dotenv';
+dotenv.config()
+
 const app = express();
-const port = 8080;
+const port = process.env['LOCAL_PORT'];
 
 app.use(express.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.post('/api/create', async (req, res) => {
     try {
-        await userRepository.createUser(User.getFromJson(req.body).encryptPassword());
+        let user = User.getFromJson(req.body);
+        user.encryptPassword();
+        await userRepository.createUser(user);
         res.sendStatus(200);
     } catch(error) {
         console.log(error);
